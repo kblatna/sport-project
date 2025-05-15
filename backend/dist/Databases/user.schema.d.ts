fa5987b9db35a605958606b0218b2051e41471ae
Katerina Blatna <39617047+kblatna@users.noreply.github.com>
2025-05-15 14:40:38 +0200

    Delete backend/dist directory

--- backend/dist/Databases/user.schema.d.ts
+++
@@ -1,9 +0,0 @@
-import { type HydratedDocument } from 'mongoose';
-export declare class User {
-    name: string;
-    email: string;
-    createdAt: Date;
-}
-export declare const UserSchema: any;
-export type UserDocument = HydratedDocument<User>;
-export type UserLeanDocument = LeanDocument<UserDocument>;
