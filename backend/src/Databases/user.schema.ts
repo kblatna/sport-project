db25de2b14c0d8df39dcaedf5e1884521e4730bf
blatnak <katerina.blatna@email.cz>
2025-05-16 09:28:04 +0200

    feat(be): user schema - version key false

--- backend/src/Databases/user.schema.ts
+++ backend/src/Databases/user.schema.ts
@@ -2,24 +2,36 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { SchemaTypes, type HydratedDocument } from 'mongoose'
 
 @Schema({
-  minimize: false
+    minimize: false,
+    versionKey: false
 })
 export class User {
-  @Prop({
-    type: SchemaTypes.String
-  })
-  name: string
+    @Prop({
+        type: SchemaTypes.String,
+        required: true
+    })
+    name: string
 
-  @Prop({
-    type: SchemaTypes.String
-  })
-  email: string
+    // @Prop({
+    //     type: SchemaTypes.String,
+    //     required: true,
+    //     index: {
+    //         unique: true
+    //     },
+    // })
+    // username: string
 
-  @Prop({
-    type: SchemaTypes.Date,
-    default: () => new Date()
-  })
-  createdAt: Date
+    @Prop({
+        type: SchemaTypes.String,
+        required: true
+    })
+    email: string
+
+    @Prop({
+        type: SchemaTypes.Date,
+        default: () => new Date()
+    })
+    createdAt: Date
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
