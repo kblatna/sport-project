48aa7b862558c429b0700aa14bff9f4a5d285cee
blatnak <katerina.blatna@email.cz>
2025-06-05 15:17:43 +0200

    refactor(fe): vite.config refactor

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    plugins: [
+        vue(),
+        vueDevTools()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
     server: {
         proxy: {
