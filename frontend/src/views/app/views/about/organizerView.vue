ae46d949a8557d557b55012c8028cacfc882837d
blatnak <katerina.blatna@email.cz>
2025-06-18 16:19:48 +0200

    feat(fe): Organizer page content added

--- frontend/src/views/app/views/about/organizerView.vue
+++ frontend/src/views/app/views/about/organizerView.vue
@@ -1,7 +1,34 @@
 <template>
-    <div class="organizer-view">
-        <h1>Organizer View</h1>
-        <p>This is the organizer view.</p>
+    <div class="container">
+        <h1>Slovo o pořadateli</h1>
+
+        <p>
+            V&nbsp;současné době je pořadatelem závodu spolek Defekt. Skutečnými pořadateli jsou již od počátků stále titíž lidé,
+            do roku 1999 pod hlavičkou Vysokoškolského sportovního klubu Mendelovy zemědělské a&nbsp;lesnické univerzity (který
+            s&nbsp;vlastní organizací závodu neměl nic společného, nicméně umožňoval vyúčtování veškerých finančních transakcí
+            a&nbsp;zajištění potřebného „papírování“). Roku 2000 se pořadatelé rozhodli založit občanské sdružení, které by
+            převzalo veškerou agendu Soběšické Muldy a&nbsp;pořádalo závod pod svou hlavičkou. V&nbsp;roce 2014 se právní forma
+            změnila na „zapsaný spolek“ (z.s.).
+        </p>
+
+        <p>Jediným pořadatelem závodu je z.s. Defekt. Více informací o&nbsp;tomto spolku naleznete například zde.</p>
+
+        <h3>Organizační tým</h3>
+        <ul>
+            <li>Ing. Zdeněk Bartoš</li>
+            <li>Bc. Miroslav Borkovec</li>
+            <li>Mgr. Jana Dojivová</li>
+            <li>Mgr. Jan Hradil, Ph.D.</li>
+            <li>prof. RNDr. Jan Strejček, Ph.D.</li>
+            <li>Ing. Pavel Strejček</li>
+        </ul>
+
+        <h3>Poděkování</h3>
+        <p>
+            Na pořádání Soběšické Muldy se podílí nepočítaně „bezejmenných“ pořadatelů - naši kamarádi a&nbsp;známí, rodinní příslušníci výše uvedeného organizačního týmu a&nbsp;jejich známí, atd.
+            Ti se nám rozhodli zcela nezištně při pořádání závodu pomoci a&nbsp;bez jejichž účasti by nebylo v&nbsp;lidských silách hlavního šestičlenného pořadatelského týmu závod uspořádat.
+            Nemůžeme zde všechny pořadatele vyjmenovat, protože bychom určitě na někoho zapomněli, ale tímto jim všem vyjadřujeme obrovský DÍK.
+        </p>
     </div>
 </template>
 
