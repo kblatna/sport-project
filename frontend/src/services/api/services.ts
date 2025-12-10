dd8e6115df54eaee19975ce5f23378f4bf101230
kblatna <katerina.blatna@email.cz>
2025-12-10 15:15:07 +0100

    fix(fe): services - link fix

--- frontend/src/services/api/services.ts
+++ frontend/src/services/api/services.ts
@@ -4,7 +4,7 @@ export const users = createService('users')
 export const results = createService('results')
 export const mainPageContent = createService('main-page-content')
 export const resultPageContent = createService('result-page-content')
-export const organizersPageContent = createService('organizers-page-content')
+export const organizersPageContent = createService('organizer-page-content')
 export const linksPageContent = createService('links-page-content')
 
 export function listResults(query?: Record<string, unknown>) {
