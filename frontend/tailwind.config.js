294fbf6323e85e85a4a0dba0412c3e0e46a81868
kblatna <katerina.blatna@email.cz>
2025-12-17 14:42:06 +0100

    fix(fe): Tailwind.config - redundant settings removed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -24,20 +24,6 @@ export default {
             },
             fontFamily: {
                 montserrat: ['Montserrat', 'sans-serif']
-            },
-            container: {
-                center: true,
-                padding: {
-                    DEFAULT: '1rem',
-                    lg: '2rem'
-                },
-                screens: {
-                    'sm': '640px',
-                    'md': '768px',
-                    'lg': '1024px',
-                    'xl': '1280px',
-                    '2xl': '1440px'
-                }
             }
         }
     },
