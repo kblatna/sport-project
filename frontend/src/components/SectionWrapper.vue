ad7519f101e13932a9b33e34aacff309483d0644
kblatna <katerina.blatna@email.cz>
2025-12-22 16:23:50 +0100

    fix(fe): SectionWrapper - default value changed

--- frontend/src/components/SectionWrapper.vue
+++ frontend/src/components/SectionWrapper.vue
@@ -25,7 +25,7 @@ interface Props {
 
 const props = withDefaults(defineProps<Props>(), {
     background: 'white',
-    paddingY: 'lg',
+    paddingY: 'md',
     customClass: '',
     constrain: true,
     innerClass: 'max-w-content mx-auto px-4'
