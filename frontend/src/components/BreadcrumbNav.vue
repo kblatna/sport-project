321b73f656699c2873c25db0267df2672d0230aa
kblatna <katerina.blatna@email.cz>
2026-01-05 12:12:51 +0100

    fix(fe): Breadcrumb correction - usage of router-link

--- frontend/src/components/BreadcrumbNav.vue
+++ frontend/src/components/BreadcrumbNav.vue
@@ -7,7 +7,58 @@
             :home="home"
             :model="items"
             class="[&_.p-breadcrumb-list]:items-center [&_.pi-home]:text-lg"
-        />
+        >
+            <!-- Links rewriting to use router-link -->
+            <template #item="{ item, props }">
+                <router-link
+                    v-if="item === home"
+                    v-slot="{ href, navigate }"
+                    :to="{ name: 'Home' }"
+                    custom
+                >
+                    <a
+                        :href="href"
+                        v-bind="props.action"
+                        @click="navigate"
+                    >
+                        <span :class="item.icon"></span>
+                        <span>{{ item.label }}</span>
+                    </a>
+                </router-link>
+
+                <router-link
+                    v-else-if="item.to"
+                    v-slot="{ href, navigate }"
+                    :to="item.to"
+                    custom
+                >
+                    <a
+                        :href="href"
+                        v-bind="props.action"
+                        @click="navigate"
+                    >
+                        <span
+                            :class="item.icon"
+                            v-if="item.icon"
+                        ></span>
+                        <span>{{ item.label }}</span>
+                    </a>
+                </router-link>
+
+                <!-- If there is a `url` (external link) -->
+                <a
+                    v-else
+                    :href="item.url"
+                    v-bind="props.action"
+                >
+                    <span
+                        :class="item.icon"
+                        v-if="item.icon"
+                    ></span>
+                    <span>{{ item.label }}</span>
+                </a>
+            </template>
+        </Breadcrumb>
     </div>
 </template>
 
@@ -20,8 +71,7 @@ const route = useRoute()
 
 const home = {
     icon: 'pi pi-home',
-    label: 'Domů',
-    to: '/'
+    label: 'Domů'
 }
 
 const items = computed(() => {
@@ -31,9 +81,15 @@ const items = computed(() => {
 
     return route.matched
         .filter(r => r.meta.breadcrumb && r.name)
-        .map(r => ({
-            label: r.meta.breadcrumb as string,
-            to: r.path
-        }))
+        .map((r) => {
+            const target = typeof r.name === 'string'
+                ? { name: r.name as string, params: route.params }
+                : r.path
+
+            return {
+                label: r.meta.breadcrumb as string,
+                to: target
+            }
+        })
 })
 </script>
